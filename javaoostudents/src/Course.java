6b20055c9eaa66bd118b2693d5054b71b2e54efe
jml19 <jessica.lally19@imperial.ac.uk>
2020-03-18 16:05:55 +0000

    Completed first two parts of Students

--- javaoostudents/src/Course.java
+++ javaoostudents/src/Course.java
@@ -1,5 +1,29 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Course {
 
 	/* WRITE YOUR CODE HERE */
+
+  private Set<Student> students;
+  private String name;
+
+  public Course(String name, Set<Student> students){
+    this.students = students;
+    this.name = name;
+  }
+
+  public Set<Postgraduate> getPostgraduates(String nameOfSupervisor){
+    Set<Postgraduate> postgraduates = new HashSet<>();
+    for (Student student : students){
+      if (student instanceof Postgraduate){
+        Postgraduate postgraduate = (Postgraduate) student;
+        if (postgraduate.getSupervisor().getName().equals(nameOfSupervisor)){
+          postgraduates.add(postgraduate);
+        }
+      }
+    }
+    return postgraduates;
+  }
 
 }
--- javaoostudents/src/Postgraduate.java
+++ javaoostudents/src/Postgraduate.java
@@ -1,5 +1,17 @@
 
 public class Postgraduate extends Student{
-// WRITE YOUR CODE HERE	
+// WRITE YOUR CODE HERE
+
+  private Academic supervisor;
+
+  public Postgraduate(String login, String name, String email, Academic supervisor){
+    super(login, name, email);
+    this.supervisor = supervisor;
+  }
+
+  public Academic getSupervisor(){
+    return supervisor;
+  }
+
 	
 }
